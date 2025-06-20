ba95b04d9df3dadcafc9b97082d0ef74da1d4d26
aalexb291 <alexander.bowie@student.adelaide.edu.au>
2025-06-20 06:23:15 +0000

    [automatic commit] 2025-06-20 06:23:15 UTC 45.249.118.41

--- part1/app.js
+++ part1/app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var apiRouter = require('./routes/api');
+// var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -17,6 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api', apiRouter);
+// app.use('/api', apiRouter);
 
 module.exports = app;
