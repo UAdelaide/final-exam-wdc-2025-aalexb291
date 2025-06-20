4b5aaa4a94767bc70b4ce9c3519780be5c78287a
aalexb291 <alexander.bowie@student.adelaide.edu.au>
2025-06-20 06:00:24 +0000

    [automatic commit] 2025-06-20 06:00:24 UTC 45.249.118.41

--- part1/database/insertqueries.sql
+++ part1/database/insertqueries.sql
@@ -17,7 +17,8 @@ VALUES
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
 VALUES
-((SELECT dog_id FROM Dogs WHERE name='Max'), '2025-06-10 08:00:00', 30)
+((SELECT dog_id FROM Dogs WHERE name='Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+((SELECT dog_id FROM Dogs WHERE name='Bella'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
 
 
 /*
