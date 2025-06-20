c74a8d97933de490a58c36ce975900668c5a957c
aalexb291 <alexander.bowie@student.adelaide.edu.au>
2025-06-20 05:59:23 +0000

    [automatic commit] 2025-06-20 05:59:23 UTC 45.249.118.41

--- part1/database/insertqueries.sql
+++ part1/database/insertqueries.sql
@@ -10,14 +10,14 @@ VALUES
 INSERT INTO Dogs (owner_id, name, size)
 VALUES
 ((SELECT user_id FROM Users WHERE username='alice123'), 'Max', 'medium'),
-((SELECT user_id from Users WHERE username='carol123'), 'Bella', 'small'),
-((SELECT user_id from Users WHERE username='carol123'), 'Foxy', 'large'),
-((SELECT user_id from Users WHERE username='alexb'), 'Kevin', 'small'),
-((SELECT user_id from Users WHERE username='alexb'), 'Steve', 'medium');
+((SELECT user_id FROM Users WHERE username='carol123'), 'Bella', 'small'),
+((SELECT user_id FROM Users WHERE username='carol123'), 'Foxy', 'large'),
+((SELECT user_id FROM Users WHERE username='alexb'), 'Kevin', 'small'),
+((SELECT user_id FROM Users WHERE username='alexb'), 'Steve', 'medium');
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
 VALUES
-((SELECT dog_id FROM ))
+((SELECT dog_id FROM Dogs WHERE name='Max'))
 
 
 /*
