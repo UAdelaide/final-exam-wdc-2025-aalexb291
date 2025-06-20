c8f4d87cd9511972631631af39b0583021376f35
aalexb291 <alexander.bowie@student.adelaide.edu.au>
2025-06-20 05:43:01 +0000

    [automatic commit] 2025-06-20 05:43:01 UTC 45.249.118.41

--- part1/database/insertqueries.sql
+++ part1/database/insertqueries.sql
@@ -1,8 +1,10 @@
 USE DogWalkService;
 INSERT INTO Users
 VALUES
-()
+('alice123', 'alice@example.com')
 
 /*
-
+A user with the username alice123, email alice@example.com, password hash hashed123, and role owner.
+A user with the username bobwalker, email bob@example.com, password hash hashed456, and role walker.
+A user with the username carol123, email carol@example.com, password hash hashed789, and role owner.
 */
