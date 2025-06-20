04f25f9ea57452f232608dfd9a3e974eff742788
aalexb291 <alexander.bowie@student.adelaide.edu.au>
2025-06-20 08:02:58 +0000

    [automatic commit] 2025-06-20 08:02:58 UTC 45.249.118.41

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -81,8 +81,8 @@ router.get('/walkrequests/open', async (req, res) => {
             SELECT WalkRequests.request_id, Dogs.name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username
             FROM WalkRequests
             JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
-            JOIN Users ON Dogs.owner_id = Users.user_id;
-            WHERE WalkRequests.status='open'
+            JOIN Users ON Dogs.owner_id = Users.user_id
+            WHERE WalkRequests.status='open';
         `;
 
         const [result] = await db.query(query);
