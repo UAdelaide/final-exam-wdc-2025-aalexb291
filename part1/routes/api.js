8f349ff24567da4ebbe9d99de947db58fa37d1c0
aalexb291 <alexander.bowie@student.adelaide.edu.au>
2025-06-20 06:15:01 +0000

    [automatic commit] 2025-06-20 06:15:00 UTC 45.249.118.41

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 
+// Connect snippet adapted from ExpressJS documentation at https://expressjs.com/en/guide/database-integration.html#mysql
 var connection = mysql.createConnection({
   database: 'DogWalkService'
 });
