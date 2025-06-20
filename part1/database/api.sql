71b79d01c278c0aaad68c07dee2049be3104e83e
aalexb291 <alexander.bowie@student.adelaide.edu.au>
2025-06-20 08:29:49 +0000

    [automatic commit] 2025-06-20 08:29:49 UTC 45.249.118.41

--- part1/database/api.sql
+++ part1/database/api.sql
@@ -6,7 +6,7 @@ JOIN Users ON Dogs.owner_id = Users.user_id;
 
 -- Walk Requests
 
-SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username
+SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner_username
 FROM WalkRequests
 JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
 JOIN Users ON Dogs.owner_id = Users.user_id
@@ -14,7 +14,7 @@ WHERE WalkRequests.status='open';
 
 -- Walker Summary
 
-SELECT Users.user_id, COUNT(WalkRatings.request_id) AS total_ratings, AVG(WalkRatings.rating) AS average_rating, COUNT(WalkRatings.request_id) AS completed_walks
+SELECT Users.user_id AS walker_username, COUNT(WalkRatings.request_id) AS total_ratings, AVG(WalkRatings.rating) AS average_rating, COUNT(WalkRatings.request_id) AS completed_walks
 FROM WalkRatings
 JOIN Users ON WalkRatings.walker_id = Users.user_id
 GROUP BY WalkRatings.walker_id, Users.username;
