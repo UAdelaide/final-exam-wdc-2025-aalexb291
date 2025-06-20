1bf7839af86caca0a5f3c370c238c340e651360b
aalexb291 <alexander.bowie@student.adelaide.edu.au>
2025-06-20 05:44:43 +0000

    [automatic commit] 2025-06-20 05:44:43 UTC 45.249.118.41

--- part1/database/insertqueries.sql
+++ part1/database/insertqueries.sql
@@ -3,7 +3,7 @@ INSERT INTO Users (username, email, password_hash, role)
 VALUES
 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-('alice123', 'alice@example.com', 'hashed123', 'owner'),
+('carol123', 'carol@example.com', 'hashed789', 'owner'),
 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 ('alice123', 'alice@example.com', 'hashed123', 'owner');
 
