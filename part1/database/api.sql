30bb14de625fe82594c68ba2b85d2ce962fd6195
aalexb291 <alexander.bowie@student.adelaide.edu.au>
2025-06-20 08:29:29 +0000

    [automatic commit] 2025-06-20 08:29:29 UTC 45.249.118.41

--- part1/database/api.sql
+++ part1/database/api.sql
@@ -6,7 +6,7 @@ JOIN Users ON Dogs.owner_id = Users.user_id;
 
 -- Walk Requests
 
-SELECT WalkRequests.request_id, Dogs.name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username
+SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username
 FROM WalkRequests
 JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
 JOIN Users ON Dogs.owner_id = Users.user_id
