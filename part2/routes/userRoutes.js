54c9181412b8dd7ed220b16b65279a7f27e58ec2
aalexb291 <alexander.bowie@student.adelaide.edu.au>
2025-06-21 04:51:39 +0000

    [automatic commit] 2025-06-21 04:51:39 UTC 45.249.118.41

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
-const session = require('express-session');
 
 // GET all users (for admin/testing)
 router.get('/', async (req, res) => {
@@ -50,7 +49,7 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
-    req.session.user
+    req.session.
 
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
