8c139082b23d83a6ce53e63b20ca1075575a4dff
aalexb291 <alexander.bowie@student.adelaide.edu.au>
2025-06-21 04:46:24 +0000

    [automatic commit] 2025-06-21 04:46:24 UTC 45.249.118.41

--- part2/app.js
+++ part2/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const path = require('path');
-
+const session = require('express-session');
 require('dotenv').config();
 
 const app = express();
 
 // Middleware
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false }
+}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
