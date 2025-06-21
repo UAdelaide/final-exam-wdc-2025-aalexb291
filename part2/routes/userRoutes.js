382bb99e09bcbae6bb6b62a3fcc1dd3f21cb4184
aalexb291 <alexander.bowie@student.adelaide.edu.au>
2025-06-21 05:19:19 +0000

    [automatic commit] 2025-06-21 05:19:19 UTC 45.249.118.41

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -37,7 +37,7 @@ router.get('/me', (req, res) => {
 
 // POST login (dummy version)
 router.post('/login', async (req, res) => {
-  console.log("HI")
+  console.log("HI");
   const { username, password_hash } = req.body;
 
   try {
